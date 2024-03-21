387e951c3ff86f7fa88091a98d0a81fad4035c4a
chukusuccess <successchuku5@gmail.com>
2024-03-21 21:35:35 +0100

    added suspense to home and pathform

--- app/home/page.js
+++ app/home/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "antd";
 import { ProposalCard } from "../components/ProposalCard";
@@ -47,37 +48,45 @@ export default function PathPage() {
   ];
 
   return (
-    <div className="w-full h-full px-4 py-24 flex flex-col items-center justify-center gap-5">
-      <div className="w-full">
-        <h1 className="font-semibold text-xl mb-1">
-          {title ? title : "Proposed Grants"}
-        </h1>
-        {/* <p className="text-xs font-light">This step is optional!</p> */}
+    <Suspense
+      fallback={
+        <div className="w-screen h-screen bg-[#fafafa] text-[#333]">
+          Loading...
+        </div>
+      }
+    >
+      <div className="w-full h-full px-4 py-24 flex flex-col items-center justify-center gap-5">
+        <div className="w-full">
+          <h1 className="font-semibold text-xl mb-1">
+            {title ? title : "Proposed Grants"}
+          </h1>
+          {/* <p className="text-xs font-light">This step is optional!</p> */}
+        </div>
+        <div className="flex flex-col gap-5 items-center w-full">
+          {proposedGrants.map((item, index) => {
+            return (
+              <ProposalCard
+                key={index}
+                title={item.title}
+                description={item.description}
+                location={item.location}
+                owner={item.owner}
+              />
+            );
+          })}
+        </div>
+        <div className="fixed bottom-0 py-4 font-extralight text-xs bg-[#ffffff90] backdrop-blur-2xl w-full flex flex-col items-center gap-3">
+          <Button
+            className="w-5/6 text-green-500 border border-green-500 text-lg"
+            size="large"
+          >
+            Write a Proposal
+          </Button>
+          <p>
+            powered by <b>impact stream</b>
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col gap-5 items-center w-full">
-        {proposedGrants.map((item, index) => {
-          return (
-            <ProposalCard
-              key={index}
-              title={item.title}
-              description={item.description}
-              location={item.location}
-              owner={item.owner}
-            />
-          );
-        })}
-      </div>
-      <div className="fixed bottom-0 py-4 font-extralight text-xs bg-[#ffffff90] backdrop-blur-2xl w-full flex flex-col items-center gap-3">
-        <Button
-          className="w-5/6 text-green-500 border border-green-500 text-lg"
-          size="large"
-        >
-          Write a Proposal
-        </Button>
-        <p>
-          powered by <b>impact stream</b>
-        </p>
-      </div>
-    </div>
+    </Suspense>
   );
 }
